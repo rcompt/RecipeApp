4d318d5c4385ba0da5f0b4d66b104619b7fd46d2
rcompt <rcompton@ucsc.edu>
2018-09-17 21:37:54 -0700

    CRUD routes added

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-const express           = require('express');
-const MongoClient       = require('mongodb').MongoClient;
-const bodyParser        = require('body-parser');
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const db             = require('./config/db');
 
-const app               = express();
+const app            = express();
 
 const port = 8000;
 
-app.use(bodyParser.urlencoded({ extended: true}));
+var fs = require('fs');
+var https = require('https');
 
-require('./app/routes')(app, {});
-app.listen(port, () => {
-    console.log("We are live on " + port);
+var key = fs.readFileSync('encryption/private.key');
+var cert = fs.readFileSync('encryption/recipes.csr');
+
+var options = {
+  key: key,
+  cert: cert
+}
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(db.url, function(err, client) {
+  if(err) {
+    console.log(err);
+  }else{
+    console.log("Connected to DB");
+  }
+  var db = client.db("recipes")
+  require('./app/routes')(app, db);
+
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });
 });
